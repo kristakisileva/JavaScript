16255ffacc91883304d582bbec57e0a9ba21bb7e
kristakisileva <140539781+kristakisileva@users.noreply.github.com>
2023-08-05 12:53:05 +0300

    Add files via upload

---
+++ uzdevums.js
@@ -0,0 +1,30 @@
+let firstName = "Krista";
+let age = 34;
+let isStudent = true;
+let fruits = ["orange", "banana", "kivi"];
+let person = {
+    personName: "Marija",
+    personAge: 8,
+    personIsStudent: false
+}; 
+
+console.log (firstName, age, isStudent);
+console.log (fruits);
+
+function myFunction() {
+    let x= 5;
+    console.log(x);
+}
+myFunction();
+
+let y= 10;
+function logY() {
+console.log(y);
+}
+logY();
+
+function task6 () {
+    let z = 15;
+    console.log(z); 
+}
+task6()
